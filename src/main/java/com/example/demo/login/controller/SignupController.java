eada15da8fb4a2ce5d90e29a07e2f2ed4d0357eb
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-06-24 08:33:13 +0900

    Implement till 6.3.1

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -8,6 +8,7 @@ import com.example.demo.login.domain.model.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,7 +53,7 @@ public class SignupController {
      * ユーザー登録画面のPOSTメソッド用処理.
      */
     @PostMapping("/signup")
-    public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+    public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
 
         // 入力チェックに引っかかった場合、ユーザー登録画面に戻る
         if (bindingResult.hasErrors()) {
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -1,19 +1,44 @@
 package com.example.demo.login.domain.model;
 
 import java.util.Date;
+
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
 public class SignupForm {
 
+    private static final int _100 = 100;
+
+    @NotBlank
+    @Email
     private String userId; // ユーザーID
+
+    @NotBlank
+    @Length(min = 4, max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password; // パスワード
+
+    @NotBlank
     private String userName; // ユーザー名
 
+    @NotNull
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private Date birthday; // 誕生日
 
+    @Min(20)
+    @Max(100)
     private int age; // 年齢
+
+    @AssertFalse
     private boolean marriage; // 結婚ステータス
 }
