5b8ffb0bc6d5359f0e1f143d01ae9983df505c57
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-06-30 19:53:58 +0900

    Implement till 10.3.3

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,32 +14,51 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+        // データソース
+        @Autowired
+        private DataSource dataSource;
 
-        // 静的リソースへのアクセスには、セキュリティを適用しない
-        web.ignoring().antMatchers("/webjars/∗∗", "/css/∗∗");
-    }
+        // ユーザーIDとパスワードを取得するSQL文
+        private static final String USER_SQL = "SELECT" + "    user_id," + "    password," + "    true" + " FROM"
+                        + "    m_user" + " WHERE" + "    user_id = ?";
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+        // ユーザーのロールを取得するSQL文
+        private static final String ROLE_SQL = "SELECT" + "    user_id," + "    role" + " FROM" + "    m_user"
+                        + " WHERE" + "    user_id = ?";
 
-        // ログイン不要ページの設定
-        http.authorizeRequests().antMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
-                .antMatchers("/css/**").permitAll() // cssへアクセス許可
-                .antMatchers("/login").permitAll() // ログインページは直リンクOK
-                .antMatchers("/signup").permitAll() // ユーザー登録画面は直リンクOK
-                .anyRequest().authenticated(); // それ以外は直リンク禁止
+        @Override
+        public void configure(WebSecurity web) throws Exception {
 
-        // ログイン処理
-        http.formLogin().loginProcessingUrl("/login") // ログイン処理のパス
-                .loginPage("/login") // ログインページの指定
-                .failureUrl("/login") // ログイン失敗時の遷移先
-                .usernameParameter("userId") // ログインページのユーザーID
-                .passwordParameter("password") // ログインページのパスワード
-                .defaultSuccessUrl("/home", true); // ログイン成功後の遷移先
+                // 静的リソースへのアクセスには、セキュリティを適用しない
+                web.ignoring().antMatchers("/webjars/∗∗", "/css/∗∗");
+        }
 
-        // CSRF対策を無効に設定（一時的）
-        http.csrf().disable();
-    }
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+                // ログイン不要ページの設定
+                http.authorizeRequests().antMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
+                                .antMatchers("/css/**").permitAll() // cssへアクセス許可
+                                .antMatchers("/login").permitAll() // ログインページは直リンクOK
+                                .antMatchers("/signup").permitAll() // ユーザー登録画面は直リンクOK
+                                .anyRequest().authenticated(); // それ以外は直リンク禁止
+
+                // ログイン処理
+                http.formLogin().loginProcessingUrl("/login") // ログイン処理のパス
+                                .loginPage("/login") // ログインページの指定
+                                .failureUrl("/login") // ログイン失敗時の遷移先
+                                .usernameParameter("userId") // ログインページのユーザーID
+                                .passwordParameter("password") // ログインページのパスワード
+                                .defaultSuccessUrl("/home", true); // ログイン成功後の遷移先
+
+                // CSRF対策を無効に設定（一時的）
+                http.csrf().disable();
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(USER_SQL)
+                                .authoritiesByUsernameQuery(ROLE_SQL);
+        }
+
 }
