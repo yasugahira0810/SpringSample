2d5082fe97c777b7bc7a1faf7579f4fbbd8eac94
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-06-30 22:47:21 +0900

    Implement till 10.3.4

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -3,16 +3,24 @@ package com.example.demo;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
         // データソース
         @Autowired
@@ -58,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
                 auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(USER_SQL)
-                                .authoritiesByUsernameQuery(ROLE_SQL);
+                                .authoritiesByUsernameQuery(ROLE_SQL).passwordEncoder(passwordEncoder());
         }
 
 }
--- src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.login.domain.model.User;
@@ -18,6 +19,9 @@ public class UserDaoJdbcImpl implements UserDao {
 
     @Autowired
     JdbcTemplate jdbc;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     // Userテーブルの件数を取得.
     @Override
@@ -32,12 +36,13 @@ public class UserDaoJdbcImpl implements UserDao {
     @Override
     public int insertOne(User user) throws DataAccessException {
 
+        String password = passwordEncoder.encode(user.getPassword());
         // １件登録
         int rowNumber = jdbc.update(
                 "INSERT INTO m_user(user_id," + " password," + " user_name," + " birthday," + " age," + " marriage,"
                         + " role)" + " VALUES(?, ?, ?, ?, ?, ?, ?)",
-                user.getUserId(), user.getPassword(), user.getUserName(), user.getBirthday(), user.getAge(),
-                user.isMarriage(), user.getRole());
+                user.getUserId(), password, user.getUserName(), user.getBirthday(), user.getAge(), user.isMarriage(),
+                user.getRole());
 
         return rowNumber;
     }
@@ -98,12 +103,14 @@ public class UserDaoJdbcImpl implements UserDao {
     // Userテーブルを１件更新.
     @Override
     public int updateOne(User user) throws DataAccessException {
+
+        String password = passwordEncoder.encode(user.getPassword());
+
         // １件更新
         int rowNumber = jdbc.update(
                 "UPDATE M_USER" + " SET" + " password = ?," + " user_name = ?," + " birthday = ?," + " age = ?,"
                         + " marriage = ?" + " WHERE user_id = ?",
-                user.getPassword(), user.getUserName(), user.getBirthday(), user.getAge(), user.isMarriage(),
-                user.getUserId());
+                password, user.getUserName(), user.getBirthday(), user.getAge(), user.isMarriage(), user.getUserId());
 
         return rowNumber;
     }
