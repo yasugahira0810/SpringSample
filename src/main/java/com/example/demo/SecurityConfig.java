21b5a2c75250110eff8967d073e991ac9caf907b
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-07-02 00:11:33 +0900

    Implement till 11.2

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -50,7 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                 .antMatchers("/css/**").permitAll() // cssへアクセス許可
                                 .antMatchers("/login").permitAll() // ログインページは直リンクOK
                                 .antMatchers("/signup").permitAll() // ユーザー登録画面は直リンクOK
-                                .antMatchers("/admin").hasAuthority("ROLE_ADMIN").anyRequest().authenticated(); // それ以外は直リンク禁止
+                                .antMatchers("/rest/**").permitAll() // RESTは直リンクOK
+                                .antMatchers("/admin").hasAuthority("ROLE_ADMIN") // /adminはADMIN権限のみOK
+                                .anyRequest().authenticated(); // それ以外は直リンク禁止
 
                 // ログイン処理
                 http.formLogin().loginProcessingUrl("/login") // ログイン処理のパス
@@ -66,6 +69,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // CSRF対策を無効に設定（一時的）
                 // http.csrf().disable();
+
+                // CSRFを無効にするURLを設定
+                RequestMatcher csrfMatcher = new RestMatcher("/rest/**");
+
+                // RESTのみCSRF対策を無効に設定
+                http.csrf().requireCsrfProtectionMatcher(csrfMatcher);
         }
 
         @Override
